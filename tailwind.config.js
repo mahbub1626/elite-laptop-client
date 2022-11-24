e8a891b38fbea23a0bc2254a8418a5245c367a04
mahbub1626 <mahbub1626@gmail.com>
2022-11-24 18:39:53 +0600

    login page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        elitethemes: {
+
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#021431",
+
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
